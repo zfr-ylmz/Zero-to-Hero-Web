889a39f6aae97f6972def69cc34b34917440e477
Zafer YILMAZ <zfrylmz4@gmail.com>
2023-01-17 22:01:03 +0300

    Konsol dısarıdan çaıştı

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+const isim = "Zafer";
+var soyisim = "Yılmaz";
+var tamisim = isim + " " +soyisim;
+//var yetiskinmi - yas!=="18";
+var buYil = new Date().getFullYear();
+//isim = "Mehmet";
+console.log(!true);
+console.log("Selam Dünyalı");
+console.log(isim,soyisim);
